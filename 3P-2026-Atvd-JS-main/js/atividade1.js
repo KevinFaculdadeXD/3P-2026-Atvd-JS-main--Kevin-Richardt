d9560ddd7cd9bed9df0d5cc50a436f8c5f824cc5
KevinR_XD69 <engs-kevinrichardt@camporeal.edu.br>
2026-04-23 22:06:45 -0300

    atividade 1 feita

--- 3P-2026-Atvd-JS-main/js/atividade1.js
+++ 3P-2026-Atvd-JS-main/js/atividade1.js
@@ -1,1 +1,11 @@
-const tituloFodaaa = document.querySelector('#tituloFodaaa')
+const tituloFodaKK = document.getElementById('tituloFodaaa');
+const botaoFodaKK = document.getElementById('apertaMeuButaoUwU');
+const subtituloFodaKK = document.getElementById('subtituloFodaaa');
+
+botaoFodaKK.addEventListener('click', () => {
+    console.log("Apertaram meu botao :("); 
+    
+    tituloFodaKK.textContent = "TROXA, CAIU NA PEGADINHA KKKKKK";
+    subtituloFodaKK.textContent = "SOBROU NADA PARA O BETA";
+    botaoFodaKK.textContent = "Betinha";
+});
